b4640e05a985668dcf64563ce3747305f5e597c5
jackmford <jackmitchellfordyce@gmail.com>
2026-06-16 16:20:05 -0400

    csv items

--- daybook.c
+++ daybook.c
@@ -8,23 +8,30 @@
 // they should have days
 //
 // -> a TUI is just controlled printing
-//
+#define MAX_ITEMS 32
+
 struct termios original;
 
 struct Item {
-	char *entry;
-	int selected;
+	char entry[128];
+	int checked;
 };
 
-void drawScreen(int selected) {
+void drawScreen(struct Item *items, int item_count, int selected) {
 	// \033 == ESC
 	// \033[2J clears the screen
 	// \033[H moves the cursor to the home position
 	printf("\033[2J\033[H");
-	printf("Daybook\r\n");
-	printf("%s[ ] Study C\r\n", selected == 0 ? "> " : " ");
-	printf("%s[ ] Run\r\n", selected == 1 ? "> " : " ");
-	printf("%s[ ] Read\r\n", selected == 2 ? "> " : " ");
+	printf("\033[1m-----------\033[0m\r\n"); // bold it
+	printf("\033[1m| Daybook |\033[0m\r\n"); // bold it
+	printf("\033[1m-----------\033[0m\r\n"); // bold it
+
+	for (int i = 0; i < item_count; i++) {
+		printf("%s[%c] %s\r\n",
+				selected == i ? "> " : " ",
+				items[i].checked ? 'x' : ' ',
+				items[i].entry);
+	}
 	fflush(stdout);
 
 }
@@ -32,6 +39,7 @@ void drawScreen(int selected) {
 void disableRaw() {
 	printf("\033[?25h"); // show cursor
 	fflush(stdout);
+	printf("\r\n");
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &original);
 }
 
@@ -51,33 +59,77 @@ void enableRaw() {
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+int loadItems(const char *filename, struct Item *items, int max_items) {
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL) {
+		return 0;
+	}
+
+	char line[256];
+	int count = 0;
+
+	fgets(line, sizeof(line), file);
+
+	while(fgets(line, sizeof(line), file) != NULL && count < max_items) {
+		char entry[128];
+		int checked;
+
+		if (sscanf(line, "%127[^,],%d", entry, &checked) == 2) {
+			// write string data from entry to items[entry]
+			snprintf(items[count].entry, sizeof(items[count].entry), "%s", entry);
+			items[count].checked = checked;
+			count++;
+		}
+	}
+	fclose(file);
+	return count;
+}
+
+void saveItems(const char *filename, struct Item *items, int item_count) {
+	FILE *file = fopen(filename, "w");
+
+	if (file == NULL) {
+		return;
+	}
+
+	fprintf(file, "entry,checked\n");
+
+	for (int i = 0; i < item_count; i++) {
+		fprintf(file, "%s,%d\n", items[i].entry, items[i].checked);
+	}
+
+	fclose(file);
+}
+
+
 
 // TODO: [ ] add support for custom items
-// TODO: [ ] figure out how to remove cursor
+// TODO: [x] figure out how to remove cursor
 int main(int argc, char* argv[]) {
 	enableRaw();
 	int selected = 0;
-	
-	// messing with custom items
-	// TODO: create an array of items and draw those to screen
-	struct Item t = {"Read", 0};
+	struct Item items[MAX_ITEMS];
+
+	int item_count = loadItems("dayitems.csv", items, MAX_ITEMS);
+
 	while (1) {
-		drawScreen(selected);
+		drawScreen(items, item_count, selected);
 		int c = getchar();
 		if (c == 'q') {
+			saveItems("dayitems.csv", items, item_count);
 			break;
 		}
 		if (c == 'j') {
-			// TODO: fix this to be of len items
-			if (selected<2) selected++;
+			if (selected<item_count-1) selected++;
 		}
 		if (c == 'k') {
 			if (selected > 0) selected--;
 		}
-		// TODO: add an enter that draws [x]
-		// There will need to be some amount of state here to save presses
+		if (c == ' ') {
+			items[selected].checked = !items[selected].checked;
+		}
 	}
-	printf("%s %d", t.entry, t.selected);
-	printf("\r\n");
+
 	return 0;
 }
